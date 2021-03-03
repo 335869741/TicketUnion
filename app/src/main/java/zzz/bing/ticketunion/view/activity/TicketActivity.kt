91cf6e9d38f712d92916c1d3947e6ff0903eb5e8
335869741 <q335869741@gmail.com>
2021-03-04 01:55:55 +0800

    The implementation checks to see if the application exists 实现检查是否存在应用

--- app/src/main/java/zzz/bing/ticketunion/view/activity/TicketActivity.kt
+++ app/src/main/java/zzz/bing/ticketunion/view/activity/TicketActivity.kt
@@ -1,5 +1,6 @@
 package zzz.bing.ticketunion.view.activity
 
+import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
@@ -15,10 +16,13 @@ import zzz.bing.ticketunion.R
 import zzz.bing.ticketunion.databinding.ActivityTicketBinding
 import zzz.bing.ticketunion.model.domain.TicketParcelable
 import zzz.bing.ticketunion.utils.Constant
+import zzz.bing.ticketunion.utils.LogUtils
 import zzz.bing.ticketunion.utils.UrlUtils
 import zzz.bing.ticketunion.viewmodel.TicketViewModel
+import java.lang.Exception
 
-class TicketActivity : BaseActivity<ActivityTicketBinding,TicketViewModel>() {
+class TicketActivity : BaseActivity<ActivityTicketBinding, TicketViewModel>() {
+    private var _isTaobaoInstall = false
 
     override fun getViewBinding(): ActivityTicketBinding {
         return ActivityTicketBinding.inflate(layoutInflater)
@@ -31,8 +35,6 @@ class TicketActivity : BaseActivity<ActivityTicketBinding,TicketViewModel>() {
 
     override fun initView() {
         setSupportActionBar(binding.toolbar)
-//        binding.toolbar.title = "淘口令"
-//        binding.toolbar.setTitleTextColor(0xaaaaaa)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.title = "淘口令"
@@ -40,7 +42,8 @@ class TicketActivity : BaseActivity<ActivityTicketBinding,TicketViewModel>() {
 
     override fun initData() {
         val bundle = intent.extras!!
-        val ticketParcelable = bundle.getParcelable<TicketParcelable>(Constant.KEY_TICKET_PARCELABLE)
+        val ticketParcelable =
+            bundle.getParcelable<TicketParcelable>(Constant.KEY_TICKET_PARCELABLE)
         viewModel.load(ticketParcelable!!)
         val url = UrlUtils.urlJoinHttp(ticketParcelable.pic)
         Glide.with(this)
@@ -48,19 +51,38 @@ class TicketActivity : BaseActivity<ActivityTicketBinding,TicketViewModel>() {
             .placeholder(R.drawable.ic_photo_placeholder)
             .listener(object : RequestListener<Drawable> {
                 override fun onLoadFailed(
-                    e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
                 ): Boolean {
                     return false
                 }
 
                 override fun onResourceReady(
-                    resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
                 ): Boolean {
                     binding.progressBar2.visibility = View.GONE
                     return false
                 }
             })
             .into(binding.imageTaoCode)
+
+        try {
+            val packageInfo =
+                packageManager.getPackageInfo("", PackageManager.MATCH_UNINSTALLED_PACKAGES)
+            _isTaobaoInstall = packageInfo != null
+            LogUtils.d(this,"packageInfo == > $packageInfo")
+        }catch (e :PackageManager.NameNotFoundException){
+            e.printStackTrace()
+            _isTaobaoInstall = false
+        }
+        LogUtils.d(this,"_isTaobaoInstall == > $_isTaobaoInstall")
+        binding.buttonTaoCode.text = if (_isTaobaoInstall) "打开淘宝领券" else "复制淘口令"
     }
 
     @Suppress("RedundantSamConstructor")
@@ -73,9 +95,5 @@ class TicketActivity : BaseActivity<ActivityTicketBinding,TicketViewModel>() {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return super.onSupportNavigateUp()
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
     }
 }
